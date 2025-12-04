61deb9832eefeda360ff3207b3ad8e56c4ea2aa6
Marc Leisi <marc.leisi@postfinance.ch>
2025-12-04 17:29:00 +0100

    chore: add test case for message loss with osb

--- examples/features/standard/xa-receive/src/main/java/org/apache/activemq/artemis/jms/example/XAReceiveExample.java
+++ examples/features/standard/xa-receive/src/main/java/org/apache/activemq/artemis/jms/example/XAReceiveExample.java
@@ -16,128 +16,123 @@
  */
 package org.apache.activemq.artemis.jms.example;
 
-import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-import javax.jms.XAConnection;
-import javax.jms.XAConnectionFactory;
-import javax.jms.XASession;
+import javax.jms.*;
 import javax.naming.InitialContext;
+import javax.transaction.xa.XAException;
 import javax.transaction.xa.XAResource;
 import javax.transaction.xa.Xid;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.activemq.artemis.utils.UUIDGenerator;
 
-/**
- * A simple JMS example showing the usage of XA support in JMS.
- */
 public class XAReceiveExample {
 
-   public static void main(final String[] args) throws Exception {
-      XAConnection connection = null;
-      InitialContext initialContext = null;
-      try {
-         // Step 1. Create an initial context to perform the JNDI lookup.
-         initialContext = new InitialContext();
+    public static void main(String[] args) throws Exception {
+        InitialContext context = null;
+        XAConnection connection1 = null;
+        XAConnection connection2 = null;
 
-         // Step 2. Lookup on the queue
-         Queue queue = (Queue) initialContext.lookup("queue/exampleQueue");
+        try {
+            context = new InitialContext();
+            Queue queue = (Queue) context.lookup("queue/exampleQueue");
+            XAConnectionFactory xaCf = (XAConnectionFactory) context.lookup("XAConnectionFactory");
+            ConnectionFactory cf = (ConnectionFactory) context.lookup("ConnectionFactory");
 
-         // Step 3. Perform a lookup on the XA Connection Factory
-         XAConnectionFactory cf = (XAConnectionFactory) initialContext.lookup("XAConnectionFactory");
+            // 1. step: xa strart and receive with timeout
+            System.out.println("Start xa1 receive");
+            connection1 = xaCf.createXAConnection();
+            connection1.start();
+            XASession xaSession1 = connection1.createXASession();
+            Session session1 = xaSession1.getSession();
+            MessageConsumer consumer1 = session1.createConsumer(queue);
+            XAResource xaRes1 = xaSession1.getXAResource();
+            Xid xid1 = new DummyXid("xa-test".getBytes(StandardCharsets.US_ASCII), 1, UUIDGenerator.getInstance().generateStringUUID().getBytes());
+            System.out.println("xa start xid1");
+            xaRes1.start(xid1, XAResource.TMNOFLAGS);
+            System.out.println("Waiting up to 10 minutes for message (terminate pod now!)");
+            Message msgBeforeStop = consumer1.receive(120000);
 
-         // Step 4.Create a JMS XAConnection
-         connection = cf.createXAConnection();
+            if (msgBeforeStop == null) {
+                System.out.println("No message received");
+            } else {
+                System.out.println("Received message: " + ((TextMessage) msgBeforeStop).getText());
+            }
 
-         // Step 5. Start the connection
-         connection.start();
+            // Wait for broker restart
+            Thread.sleep(30000);
 
-         // Step 6. Create a JMS XASession
-         XASession xaSession = connection.createXASession();
+            // 2. step: new jms connection
+            System.out.println("Restart client, create new xa session");
+            connection2 = xaCf.createXAConnection();
+            connection2.start();
+            XASession xaSession2 = connection2.createXASession();
+            Session session2 = xaSession2.getSession();
+            MessageConsumer consumer2 = session2.createConsumer(queue);
+            XAResource xaRes2 = xaSession2.getXAResource();
 
-         // Step 7. Create a normal session
-         Session normalSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            // 3. step: xa2 strart and receive with timeout
+            Xid xid2 = new DummyXid("xa-test-2".getBytes(), 1, UUIDGenerator.getInstance().generateStringUUID().getBytes());
+            System.out.println("xa start xid2");
+            xaRes2.start(xid2, XAResource.TMNOFLAGS);
 
-         // Step 8. Create a normal Message Producer
-         MessageProducer normalProducer = normalSession.createProducer(queue);
+            // 4. step: rollback old transaction within separate thread (wait at the beginning for message receive start)
+            Thread rollbackThread = new Thread(() -> {
+                try {
+                    Thread.sleep(10000);
+                    System.out.println("\n[RollbackThread] Rolling back old xid1: " + xid1);
 
-         // Step 9. Get the JMS Session
-         Session session = xaSession.getSession();
+                    try {
+                        xaRes2.end(xid1, XAResource.TMFAIL);
+                    } catch (XAException e) {
+                        e.printStackTrace();
+                    }
 
-         // Step 10. Create a message consumer
-         MessageConsumer xaConsumer = session.createConsumer(queue);
+                    try {
+                        xaRes2.rollback(xid1);
+                    } catch (XAException e) {
+                        e.printStackTrace();
+                    }
 
-         // Step 11. Create two Text Messages
-         TextMessage helloMessage = session.createTextMessage("hello");
-         TextMessage worldMessage = session.createTextMessage("world");
+                    System.out.println("[RollbackThread] Old xa rolled back");
+                    Connection connection3 = cf.createConnection();
+                    connection3.start();
+                    Session session = connection3.createSession();
+                    Message messageToSend = session.createTextMessage("New message");
+                    session.createProducer(queue).send(messageToSend);
+                    session.close();
+                    connection3.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
 
-         // Step 12. create a transaction
-         Xid xid1 = new DummyXid("xa-example1".getBytes(StandardCharsets.US_ASCII), 1, UUIDGenerator.getInstance().generateStringUUID().getBytes());
+            rollbackThread.start();
+            System.out.println("start receive message2");
+            Message message2 = consumer2.receive(120000); // 1 min
 
-         // Step 13. Get the JMS XAResource
-         XAResource xaRes = xaSession.getXAResource();
+            if (message2 == null) {
+                System.out.println("No message received");
+            } else {
+                System.out.println("Received message: " + ((TextMessage) message2).getText());
+            }
 
-         // Step 14. Begin the Transaction work
-         xaRes.start(xid1, XAResource.TMNOFLAGS);
+            try {
+                xaRes2.end(xid2, XAResource.TMSUCCESS);
+                System.out.println("End xid: " + xid2);
+                xaRes2.prepare(xid2);
+                System.out.println("Prepare xid: " + xid2);
+                xaRes2.commit(xid2, false);
+                System.out.println("Commit xid: " + xid2);
+            } catch (Exception e) {
+                e.printStackTrace();
+                xaRes2.rollback(xid2);
+                System.out.println("Rolled back xid: " + xid2);
+            }
 
-         // Step 15. Send two messages.
-         normalProducer.send(helloMessage);
-         normalProducer.send(worldMessage);
-
-         // Step 16. Receive the message
-         TextMessage rm1 = (TextMessage) xaConsumer.receive();
-         System.out.println("Message received: " + rm1.getText());
-         TextMessage rm2 = (TextMessage) xaConsumer.receive();
-         System.out.println("Message received: " + rm2.getText());
-
-         // Step 17. Stop the work
-         xaRes.end(xid1, XAResource.TMSUCCESS);
-
-         // Step 18. Prepare
-         xaRes.prepare(xid1);
-
-         // Step 19. Roll back the transaction
-         xaRes.rollback(xid1);
-
-         // Step 20. Create another transaction
-         Xid xid2 = new DummyXid("xa-example2".getBytes(), 1, UUIDGenerator.getInstance().generateStringUUID().getBytes());
-
-         // Step 21. Start the transaction
-         xaRes.start(xid2, XAResource.TMNOFLAGS);
-
-         // Step 22. receive those messages again
-         rm1 = (TextMessage) xaConsumer.receive();
-         System.out.println("Message received again: " + rm1.getText());
-         rm2 = (TextMessage) xaConsumer.receive();
-         System.out.println("Message received again: " + rm2.getText());
-
-         // Step 23. Stop the work
-         xaRes.end(xid2, XAResource.TMSUCCESS);
-
-         // Step 24. Prepare
-         xaRes.prepare(xid2);
-
-         // Step 25. Commit!
-         xaRes.commit(xid2, false);
-
-         // Step 26. Check no more messages are received.
-         TextMessage rm3 = (TextMessage) xaConsumer.receive(2000);
-         if (rm3 == null) {
-            System.out.println("No message received after commit.");
-         } else {
-            throw new IllegalStateException();
-         }
-      } finally {
-         // Step 27. Be sure to close our JMS resources!
-         if (initialContext != null) {
-            initialContext.close();
-         }
-         if (connection != null) {
-            connection.close();
-         }
-      }
-   }
+        } finally {
+            if (context != null) context.close();
+            if (connection1 != null) connection1.close();
+            if (connection2 != null) connection2.close();
+        }
+    }
 }
